bbe31ed5f6cd101238f06f30dd453376e944f86c
NHT <hoangtuan910@gmail.com>
2023-11-20 19:35:05 +0700

    update type and entity

--- src/dto/general-response.dto.ts
+++ src/dto/general-response.dto.ts
@@ -2,8 +2,10 @@ export class GeneralResponse {
   constructor(statusCode: number, message: any) {
     this.statusCode = statusCode;
     this.message = message;
+    this.data = null;
   }
 
   statusCode: number;
   message: any;
+  data: any;
 }
--- src/entity/customer.entity.ts
+++ src/entity/customer.entity.ts
@@ -20,13 +20,13 @@ export class Customer {
   public email: string;
 
   @Column({ type: 'date', nullable: true, unique: false })
-  public birthday!: Date;
+  public birthday: Date;
 
   @Column({ type: 'char', length: 1, nullable: true, unique: false })
   public sex: string;
 
   @Column({ type: 'int', nullable: true, unique: false })
-  public profile_image!: number;
+  public profile_image: number;
 
   @Column({
     type: 'char',
@@ -35,13 +35,13 @@ export class Customer {
     unique: false,
     default: '0',
   })
-  public is_active!: string;
+  public is_active: string;
 
   @Column({ type: 'int', nullable: true, unique: false })
-  public health_info_id!: number;
+  public health_info_id: number;
 
   @Column({ type: 'varchar', length: 255, nullable: true, unique: false })
-  public refresh_token!: string;
+  public refresh_token: string;
 
   @CreateDateColumn({
     type: 'datetime',
